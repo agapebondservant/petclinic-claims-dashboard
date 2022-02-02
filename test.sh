57982091d122683234971baa5f2c7c8b3004171b
Tola Awofolu <oawofolu@pivotal.io>
2022-02-02 17:15:03 -0500

    Removed file

--- test.sh
+++
@@ -1,8 +0,0 @@
-PETCLINIC_ANALYTICS_IMAGE_VERSION=`date "+%Y%m%d.%H%M"`
-docker build -t oawofolu/petclinic-analytics .
-docker push oawofolu/petclinic-analytics
-docker push oawofolu/petclinic-analytics
-kubectl delete svc petclinic-app-analytics --ignore-not-found=true
-kubectl delete deploy petclinic-app-analytics --ignore-not-found=true
-kubectl create deployment petclinic-app-analytics --image=oawofolu/petclinic-analytics:latest
-#kubectl port-forward deploy/petclinic-app-analytics 8050
